f9378b84f58bc0ae26689ce876341306981c52e7
marshallein <maxboiz123hn@gmail.com>
2022-05-30 15:46:34 +0700

    update project

--- Asm_1_PRN231/ClientWebApi/Controllers/BookController.cs
+++ Asm_1_PRN231/ClientWebApi/Controllers/BookController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -18,7 +19,7 @@ namespace ClientWebApi.Controllers
             httpClient = new HttpClient();
             var contentType = new MediaTypeWithQualityHeaderValue("application/json");
             httpClient.DefaultRequestHeaders.Accept.Add(contentType);
-            apiUrl = "https://localhost:5001/api/Books/";
+            apiUrl = "https://localhost:5001/api/Books";
         }
 
         public async Task<IActionResult> Index()
@@ -35,7 +36,51 @@ namespace ClientWebApi.Controllers
                 PropertyNameCaseInsensitive = true
             };
             List<Book> books = JsonSerializer.Deserialize<List<Book>>(data, options);
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
             return View();
         }
+
+        public async Task<IActionResult> CreateBook(Book book)
+        {
+            book.Book_id = 0;
+            HttpResponseMessage response = await httpClient.PostAsJsonAsync(apiUrl, book);
+            response.EnsureSuccessStatusCode();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int book_id)
+        {
+            var response = await httpClient.GetAsync(apiUrl + "/" + book_id);
+            string data = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            Book bookToEdit = JsonSerializer.Deserialize<Book>(data, options);
+            return View(bookToEdit);
+        }
+
+        public async Task<IActionResult> EditBook(Book book)
+        {
+            HttpResponseMessage response = await httpClient.PutAsJsonAsync<Book>(apiUrl + "/" + book.Book_id, book);
+            response.EnsureSuccessStatusCode();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteBook(int book_id)
+        {
+            HttpResponseMessage response = await httpClient.DeleteAsync(apiUrl + "/" + book_id);
+            response.EnsureSuccessStatusCode();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
